b0ee200fc2a637801e9e7e1c1f9b06b4eb3d2c57
phillippmueller <philip.mueller.bueckeburg@outlook.de>
2025-10-31 17:43:42 +0100

    file structure

---
+++ stage2_BaseModel/EPA1352-G17-A2/model/batch_run.py
@@ -0,0 +1,52 @@
+from model import BangladeshModel, compute_average_driving, compute_worst_bridge, compute_worst_bridge_delay, get_probs
+from mesa.batchrunner import FixedBatchRunner
+
+# run settings
+run_length = 5 * 24 * 60 # run the model for 5 * 24 hours, translated to minutes
+seed = 1234567
+iterations = 10 
+
+# parameters that remain constant for all runs
+fixed_params = {
+    # option for set seed "seed": seed,
+}
+
+# list with dictionaries with parameter values per scenario
+parameter_list = [
+    {"prob_A": 0, "prob_B": 0, "prob_C": 0, "prob_D": 0},
+    {"prob_A": 0, "prob_B": 0, "prob_C": 0, "prob_D": 0.05},
+    {"prob_A": 0, "prob_B": 0, "prob_C": 0, "prob_D": 0.10},
+    {"prob_A": 0, "prob_B": 0, "prob_C": 0.05, "prob_D": 0.10},
+    {"prob_A": 0, "prob_B": 0, "prob_C": 0.10, "prob_D": 0.20},
+    {"prob_A": 0, "prob_B": 0.05, "prob_C": 0.10, "prob_D": 0.20},
+    {"prob_A": 0, "prob_B": 0.10, "prob_C": 0.20, "prob_D": 0.40},
+    {"prob_A": 0.05, "prob_B": 0.10, "prob_C": 0.20, "prob_D": 0.40},
+    {"prob_A": 0.10, "prob_B": 0.20, "prob_C": 0.40, "prob_D": 0.80},
+]
+
+# configure the batch runner
+
+batch_run = FixedBatchRunner(BangladeshModel, 
+                        parameter_list, 
+                        fixed_params,
+                        iterations=iterations,
+                        max_steps=run_length,
+                        model_reporters={"Average driving time": compute_average_driving, "Worst bridge name": compute_worst_bridge, "Worst bridge delay": compute_worst_bridge_delay, "Probs": get_probs}
+                        )
+
+# run the batch run configuration and save to dataframe
+batch_run.run_all()
+
+run_data = batch_run.get_model_vars_dataframe()
+
+# save run data to csv
+run_data.to_csv('../data/experiment/all_scenarios.csv')
+
+# save every scenerio to seperate csv file
+start = 0
+end = iterations 
+
+for scenario in range(len(parameter_list)):
+    run_data[start:end].to_csv('../data/experiment/scenario{}.csv'.format(scenario))
+    start += iterations
+    end += iterations
